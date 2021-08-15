b8bd9732b88119943ba88547962ad5ab191da07f
lkm2835 <lkm2835@gmail.com>
2021-08-16 00:02:44 +0900

    add argparse

--- frozen_lake/main.py
+++ frozen_lake/main.py
@@ -5,6 +5,9 @@ import numpy as np
 import random as pr
 import matplotlib.pyplot as plt
 import math
+import argparse
+from argparse import RawTextHelpFormatter
+
 from frozen_lake import *
 
 def getChar():
@@ -42,22 +45,69 @@ def rargmax(vector):
     return pr.choice(indices)
 
 if __name__ == "__main__":
+    num_episodes = 2000
+    discounted = 0.9
+    learning_rate = 0.2
+    Q_learning_method = 1
+    slippery = True
+
+    parser = argparse.ArgumentParser(description='Parameters', formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-episodes', type=int, help='number of episodes for one game')
+    parser.add_argument('-learning_method', type=int, help=
+                            'choose Q_learning_method 1~3\n'
+                            'method 1 : use learning_rate, discounted_reward\n'
+                            'method 2 : use discounted_reward\n'
+                            'method 3 : vanila')
+    parser.add_argument('-discount', type=float, help='discount reward rate')
+    parser.add_argument('-learning_rate', type=float, help='learning rate')
+    parser.add_argument('-slippery', type=str, help='slippery')
+    args = vars(parser.parse_args())
+    
+    is_default = True
+
+    if args['episodes'] != None:
+        num_episodes = args['episodes']
+        is_default = False
+
+    if args['learning_method'] != None:
+        Q_learning_method = args['learning_method']
+        if Q_learning_method == 2 or Q_learning_method == 3:
+            is_default = False
+
+    if args['discount'] != None:
+        discounted = args['discount']
+        is_default = False
+    
+    if args['learning_rate'] != None:
+        learning_rate = args['learning_rate']
+        is_default = False
+
+    if args['slippery'] != None:
+        if args['slippery'].lower() in ('n', 'no', 'f', 'false'):
+            slippery = False
+            is_default = False
+
+    print("========================\n")
+    print("DEFAULT     ->\t", is_default)
+    print("num_episodes  :\t", num_episodes)
+    print("discount_rate :\t", discounted)
+    print("learning_rate :\t", learning_rate)
+    print("slippery_mode :\t", slippery)
+    print("\n========================\n")
+    
     pad = ['W', 'S', 'D', 'A', 'Q']
 
     games = 36
-    history = np.zeros([games, FrozenLake.env_y_, FrozenLake.env_x_], dtype = np.int32)
+    action_history = np.zeros([games, FrozenLake.env_y_, FrozenLake.env_x_], dtype = np.int32)
     Arrived_rate = np.zeros([games])
 
-    num_episodes = 10000
-    discounted = 0.9
-    learning_rate = 0.2
     for game in range(games):
         print("Games : ", game+1, "\n")
         Q = np.zeros([FrozenLake.env_y_, FrozenLake.env_x_, FrozenLake.action_n_])
         Arrived = 0
         for i in range(num_episodes):
             #print("episode : ", i + 1, "\n")
-            environment = FrozenLake(is_slippery = True)
+            environment = FrozenLake(is_slippery = slippery)
             is_game_done = False
             while not is_game_done:
                 #board.printScreen()
@@ -72,15 +122,22 @@ if __name__ == "__main__":
                     action = rargmax(Q[y_][x_][:].reshape(environment.action_n_))
                 new_y_, new_x_, reward, done = environment.accept(pad[action])
                 
-                Q[y_][x_][action] = (1 - learning_rate) * Q[y_][x_][action] + learning_rate * (reward + discounted * np.max(Q[new_y_][new_x_][:]))
+                if Q_learning_method == 1:
+                    Q[y_][x_][action] = (1 - learning_rate) * Q[y_][x_][action] + learning_rate * (reward + discounted * np.max(Q[new_y_][new_x_][:]))
+                elif Q_learning_method == 2:
+                    Q[y_][x_][action] = reward + discounted * np.max(Q[new_y_][new_x_][:])
+                elif Q_learning_method == 3:
+                    Q[y_][x_][action] = reward + np.max(Q[new_y_][new_x_][:])
+                else:
+                    Q[y_][x_][action] = (1 - learning_rate) * Q[y_][x_][action] + learning_rate * (reward + discounted * np.max(Q[new_y_][new_x_][:]))
 
                 is_game_done = done
 
             if environment.state == FrozenLakeState.Arrived:
                 Arrived += 1
-            history[game] += environment.getHistory()
+            action_history[game] += environment.getHistory()
         #print(Q, "\n")
-        print(history[game], "\n")
+        print(action_history[game], "\n")
         Arrived_rate[game] = Arrived / num_episodes * 100
-        print("Arrived :  {0:0.2f}".format(Arrived / num_episodes * 100), "%\n\n")
-    heatmapShow(history, Arrived_rate, games)
+        print("Arrived :  {0:0.2f}".format(Arrived_rate[game]), "%\n\n")
+    heatmapShow(action_history, Arrived_rate, games)
