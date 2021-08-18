4906ed5b1aa57d21d358058794515af3f4791fa2
lkm2835 <lkm2835@gmail.com>
2021-08-19 00:50:43 +0900

    setting in config.json

--- frozen_lake/main.py
+++ frozen_lake/main.py
@@ -1,6 +1,4 @@
-import sys
-import tty
-import termios
+import json
 import numpy as np
 import random as pr
 import matplotlib.pyplot as plt
@@ -9,16 +7,7 @@ import argparse
 from argparse import RawTextHelpFormatter
 
 from frozen_lake import *
-
-def getChar():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+from parse_config import ConfigParser
 
 def heatmapShow(matrix, title, num_subplots = 1, is_blocking=True, time=0):
     fig = plt.figure(figsize=(10, 10))
@@ -45,55 +34,54 @@ def rargmax(vector):
     return pr.choice(indices)
 
 if __name__ == "__main__":
-    num_episodes = 2000
-    discounted = 0.9
-    learning_rate = 0.2
-    Q_learning_method = 3
-    slippery = True
-
-    parser = argparse.ArgumentParser(description='Parameters', formatter_class=RawTextHelpFormatter)
-    parser.add_argument('-ep', '--episodes', type=int, help='number of episodes for one game')
-    parser.add_argument('-lm', '--learning_method', type=int, help=
+    args = argparse.ArgumentParser(description='Parameters', formatter_class=RawTextHelpFormatter)
+    args.add_argument('-c', '--config', default=None, type=str, help='config file path (default: None')
+    
+    config = ConfigParser.from_args(args)
+    '''
+    args.add_argument('-ep', '--episodes', type=int, help='number of episodes for one game')
+    args.add_argument('-lm', '--learning_method', type=int, help=
                             'choose Q_learning_method 1~3\n'
                             'method 1 : use learning_rate, discounted_reward\n'
                             'method 2 : use discounted_reward\n'
                             'method 3 : vanila')
-    parser.add_argument('-dr', '--discount_rate', type=float, help='discount reward rate')
-    parser.add_argument('-lr', '--learning_rate', type=float, help='learning rate')
-    parser.add_argument('-sl','--slippery', type=str, help='slippery')
-    args = vars(parser.parse_args())
+    args.add_argument('-dr', '--discount_rate', type=float, help='discount reward rate')
+    args.add_argument('-lr', '--learning_rate', type=float, help='learning rate')
+    args.add_argument('-sl','--slippery', type=str, help='slippery')
+    args = vars(args.parse_args())
     
     is_default = True
 
     if args['episodes'] != None:
-        num_episodes = args['episodes']
+        config['num_episodes'] = args['episodes']
         is_default = False
 
     if args['learning_method'] != None:
-        Q_learning_method = args['learning_method']
-        if Q_learning_method == 1 or Q_learning_method == 2:
+        config['Q_learning_method'] = args['learning_method']
+        if config['Q_learning_method'] == 1 or config['Q_learning_method'] == 2:
             is_default = False
 
     if args['discount_rate'] != None:
-        discounted = args['discount_rate']
+        config['discounted'] = args['discount_rate']
         is_default = False
     
     if args['learning_rate'] != None:
-        learning_rate = args['learning_rate']
+        config['learning_rate'] = args['learning_rate']
         is_default = False
 
     if args['slippery'] != None:
         if args['slippery'].lower() in ('n', 'no', 'f', 'false'):
-            slippery = False
+            config['slippery'] = False
             is_default = False
-
+    '''
+    
     print("========================\n")
     print("DEFAULT      ->\t", is_default)
-    print("learning_method:\t", Q_learning_method)
-    print("num_episodes   :\t", num_episodes)
-    print("discount_rate  :\t", discounted)
-    print("learning_rate  :\t", learning_rate)
-    print("slippery_mode  :\t", slippery)
+    print("learning_method:\t", config['Q_learning_method'])
+    print("num_episodes   :\t", config['num_episodes'])
+    print("discount_rate  :\t", config['discounted'])
+    print("learning_rate  :\t", config['learning_rate'])
+    print("slippery_mode  :\t", config['slippery'])
     print("\n========================\n")
     
     pad = ['W', 'S', 'D', 'A', 'Q']
@@ -106,15 +94,10 @@ if __name__ == "__main__":
         print("Games : ", game+1, "\n")
         Q = np.zeros([FrozenLake.env_y_, FrozenLake.env_x_, FrozenLake.action_n_])
         Arrived = 0
-        for i in range(num_episodes):
-            #print("episode : ", i + 1, "\n")
-            environment = FrozenLake(is_slippery = slippery)
+        for i in range(config['num_episodes']):
+            environment = FrozenLake(is_slippery = config['slippery'])
             is_game_done = False
-            while not is_game_done:
-                #board.printScreen()
-                #action = getChar()
-                #state = board.accept(action)
-                
+            while not is_game_done:                
                 y_, x_ = environment.getCurrYX()
                 e = 1 / (i + 1)
                 if np.random.random() < e:
@@ -123,14 +106,14 @@ if __name__ == "__main__":
                     action = rargmax(Q[y_][x_][:].reshape(environment.action_n_))
                 new_y_, new_x_, reward, done = environment.accept(pad[action])
                 
-                if Q_learning_method == 3:
-                    Q[y_][x_][action] = (1 - learning_rate) * Q[y_][x_][action] + learning_rate * (reward + discounted * np.max(Q[new_y_][new_x_][:]))
-                elif Q_learning_method == 2:
-                    Q[y_][x_][action] = reward + discounted * np.max(Q[new_y_][new_x_][:])
-                elif Q_learning_method == 1:
+                if config['Q_learning_method'] == 1:
                     Q[y_][x_][action] = reward + np.max(Q[new_y_][new_x_][:])
+                elif config['Q_learning_method'] == 2:
+                    Q[y_][x_][action] = reward + config['discounted'] * np.max(Q[new_y_][new_x_][:])
+                elif config['Q_learning_method'] == 3:
+                    Q[y_][x_][action] = (1 - config['learning_rate']) * Q[y_][x_][action] + config['learning_rate'] * (reward + config['discounted'] * np.max(Q[new_y_][new_x_][:]))
                 else:
-                    Q[y_][x_][action] = (1 - learning_rate) * Q[y_][x_][action] + learning_rate * (reward + discounted * np.max(Q[new_y_][new_x_][:]))
+                    Q[y_][x_][action] = (1 - config['learning_rate']) * Q[y_][x_][action] + config['learning_rate'] * (reward + config['discounted'] * np.max(Q[new_y_][new_x_][:]))
 
                 is_game_done = done
 
@@ -139,6 +122,6 @@ if __name__ == "__main__":
             action_history[game] += environment.getHistory()
         #print(Q, "\n")
         print(action_history[game], "\n")
-        Arrived_rate[game] = Arrived / num_episodes * 100
+        Arrived_rate[game] = Arrived / config['num_episodes'] * 100
         print("Arrived :  {0:0.2f}".format(Arrived_rate[game]), "%\n\n")
     heatmapShow(action_history, Arrived_rate, games)
--- frozen_lake/parse_config.py
+++ frozen_lake/parse_config.py
@@ -1,7 +1,21 @@
+import json
+
 class ConfigParser:
-    def __init__(self):
+    def __init__(self, config):
+        self.config = config
         return
 
     @classmethod
-    def from_args(cls, args, options=''):
-        return
+    def from_args(cls, args):
+        args = args.parse_args()
+        msg_no_cfg = "Configuration file need to be specified. Add '-c config.json', for example."
+        assert args.config is not None, msg_no_cfg
+
+        cfg_fname = './' + args.config
+
+        with open('./'+args.config, 'r') as json_file:
+            config = json.load(json_file)
+        return cls(config)
+
+    def __getitem__(self, name):
+        return self.config[name]
